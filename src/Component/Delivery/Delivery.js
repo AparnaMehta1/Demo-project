50d109e76de4730aa0d60fd849d097e22a81f2c8
Aparna <AparnaMehta1>
2022-11-20 20:50:17 +0530

    new changes

--- src/Component/Delivery/Delivery.js
+++ src/Component/Delivery/Delivery.js
@@ -1,106 +1,101 @@
-import React from 'react'
+import React from "react";
 
 const Delivery = () => {
   return (
     <>
-    <div>
-    <h1>Delivery</h1>
-    <hr />
-    <div class="btn-group" role="group" aria-label="Basic example">
-  <button type="button" class="btn btn-light">Free</button>
-  <button type="button" class="btn btn-light">Express:$9.99</button>
-  
-</div>
-<hr />
-<p>Delivery date : June 242022</p>
+      <div className="d-flex justify-content-center align-items-center vh-100">
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title d-flex justify-content-left mx-5 fw-bold text-muted">
+              Delivery
+            </h5>
+            <hr />
+            <div className="btn-group border border-secondary ">
+              <a
+                href="#"
+                className="btn btn-light bg-light active "
+                aria-current="page"
+              >
+                Free
+              </a>
+              <a href="#" className="btn btn-light">
+                Express:$9.99
+              </a>
+            </div>
 
-<div class="btn-group" role="group" aria-label="Basic example">
-  <button type="button" class="btn btn-light text-muted">Promocode</button>
-  <button type="button" class="btn btn-light">Apply</button>
-  
-</div>
-<p>20% off discount</p>
-<div className='row'>
-<p className=' col-6 fw-bold text-dark'>Subtotal</p>
-<p className='col-6'>$80.96</p>
+            <p className="text-muted  d-flex justify-content-left">Delivery date : June 242022</p>
+            <hr />
+            <div className="btn-group border border-secondary  form-control">
+              <a
+                href="#"
+                className="btn btn-light bg-light  active fw-bold text-muted"
+                aria-current="page"
+              >
+                Promocode
+              </a>
+              <a
+                href="#"
+                className="btn btn-light border border-secondary text-dark fw-bold"
+              >
+                Apply
+              </a>
+            </div>
 
-</div>
-<div>
-<p className='fw-bold text-muted'>Discount</p>
-<p>$80.96</p>
+            <p className="text-muted  d-flex justify-content-left">20% off discount</p>
+            <div className="row">
+              <div className="row">
+                <div className="row">
+                  <p className=" col-6 fs-4 fw-bold text-dark d-flex justify-content-left">
+                    Subtotal
+                  </p>
+                  <p className="col-6 fs-4  d-flex justify-content-end fw-bold">$80.96</p>
+                </div>
+                <div className="row">
+                  <p className=" col-6 text-dark d-flex justify-content-left">
+                    Discount
+                  </p>
+                  <p className="col-6  d-flex justify-content-end">(20%)-$16.19</p>
+                </div>
 
-</div>
-<div>
-<p className='fw-bold text-muted'>Delivery</p>
-<p>$80.96</p>
+                <div className="row">
+                  <p className=" col-6 text-dark d-flex justify-content-left">
+                    Delivery
+                  </p>
+                  <p className="col-6  d-flex justify-content-end">$0.00</p>
+                </div>
+                <div className="row">
+                  <p className=" col-6 text-dark d-flex justify-content-left">
+                    Tax
+                  </p>
+                  <p className="col-6  d-flex justify-content-end">$14.96</p>
+                </div>
+                <hr />
+                <div className="row">
+                  <p className=" col-6  fs-5 fw-bold text-dark d-flex justify-content-left">
+                    Total
+                  </p>
+                  <p className="col-6 fs-4 d-flex justify-content-end fw-bold text-dark">$78.76</p>
+                </div>
+              </div>
+            </div>
 
-</div>
-<div>
-<p className='fw-bold text-muted'>Tax</p>
-<p>$80.96</p>
+            <div>
+           <div>
+           <a href="#" className="btn btn-primary form-control">
+              Proceed to checkout
+            </a>
+           </div>
+           <div>
+           <a href="#" className="btn btn-light form-control bg-light">
+              Continue Shoping
+            </a>
+           </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-</div>
-    </div>
-    <div class="card">
-  <div class="card-header">
-    Featured
-  </div>
-  <div class="card-body">
-    <h5 class="card-title">Delivery</h5>
-    <hr />
-    <div class="btn-group" role="group" aria-label="Basic example">
-  <button type="button" class="btn btn-light d-flex justify-content-left">Free</button>
-  <button type="button" class="btn btn-light d-flex justify-content-left">Express:$9.99</button>
-  
-</div>
-<p>Delivery date : June 242022</p>
-<div class="btn-group" role="group" aria-label="Basic example">
-  <button type="button" class="btn btn-light text-muted">Promocode</button>
-  <button type="button" class="btn btn-light">Apply</button>
-  
-</div>
-<p>20% off discount</p>
-<div className='row'>
-<p className=' col-6 fw-bold text-dark'>Subtotal</p>
-<p className='col-6'>$80.96</p>
-<div className='row'>
-<p className=' col-6 fw-bold text-dark'>Discount</p>
-<p className='col-6'>$80.96</p><div className='row'>
-<p className=' col-6 fw-bold text-dark'>Subtotal</p>
-<p className='col-6 fw-bold'>$80.96</p>
-
-</div>
-<div className='row'>
-<p className=' col-6 fw-bold text-dark'>Discount</p>
-<p className='col-6'>(20%)-$16.19</p>
-
-</div>
-
-<div className='row'>
-<p className=' col-6 fw-bold text-dark'>Delivery</p>
-<p className='col-6'>$0.00</p>
-
-</div><div className='row'>
-<p className=' col-6 fw-bold text-dark'>Tax</p>
-<p className='col-6'>$14.96</p>
-
-</div>
-<div className='row'>
-<p className=' col-6 fw-bold text-dark'>Total</p>
-<p className='col-6 fw-bold text-dark'>$78.76</p>
-
-</div>
-</div>
-</div>
-
-   
-    <a href="#" class="btn btn-primary">Proceed to checkout</a>
-    <a href="#" class="btn btn-light">Continue Shoping</a>
-
-  </div>
-</div>
-</>
-  )
-}
-
-export default Delivery
+export default Delivery;
